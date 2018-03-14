d23c8199ba87ceae421dedb0774dc6d840afe7a2
critoma <cristian.toma@ie.ase.ro>
2018-03-14 12:08:36 +0200

    Modify lecture 03

---
+++ lectures/c03/buildAndRun.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export JAVA_HOME=/opt/software/java/jdks/jdk1.8.0_161
+export PATH=$JAVA_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export CLASSPATH=.:$JAVA_HOME/jre/lib
+export JSE=/home/stud/javase/lectures
+
+
+cd $JSE/c03/src
+
+javac eu/ase/polimorfism/IMiscare.java
+javac eu/ase/polimorfism/Vehicul.java
+javac eu/ase/polimorfism/IAuto.java
+javac eu/ase/polimorfism/Auto.java
+javac eu/ase/polimorfism/Avion.java
+javac eu/ase/polimorfism/ProgMain.java
+
+java eu/ase/polimorfism/ProgMain
