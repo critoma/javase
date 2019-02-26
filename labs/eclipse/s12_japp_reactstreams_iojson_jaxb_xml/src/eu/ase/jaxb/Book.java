561af90d1208ee217741ca27da2c9321b8507790
critoma <cristian.toma@ie.ase.ro>
2019-02-26 04:41:56 +0200

    refactor the labs

---
+++ labs/eclipse/s12_japp_reactstreams_iojson_jaxb_xml/src/eu/ase/jaxb/Book.java
@@ -0,0 +1,57 @@
+package eu.ase.jaxb;
+
+// one quick workaround for JDK 9 and 10:
+// --add-modules java.xml.bind in VM run configuration
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "book")
+// If you want you can define the order in which the fields are written
+// Optional
+@XmlType(propOrder = { "author", "name", "publisher", "isbn" })
+public class Book {
+
+  private String name;
+  private String author;
+  private String publisher;
+  private String isbn;
+
+  // If you like the variable name, e.g. "name", you can easily change this
+  // name for your XML-Output:
+
+  @XmlElement(name = "title")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+} 
