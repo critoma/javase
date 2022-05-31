64b192f86d03e6bd402357d57dcbfbf310e8049b
critoma <>
2022-05-31 17:22:34 +0300

    Updates for versions 17/11 for OpenJDK and JavaFX

---
+++ tools/robocode/robocode-1.9.0.0/robots/euase/jrobots/Exercices_1_1.java
@@ -0,0 +1,61 @@
+package euase.jrobots;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Exercices_1_1 - a robot by (your name here)
+ */
+public class Exercices_1_1 extends Robot
+{
+	/**
+	 * run: Exercices_1_1's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		while (true) {
+					//first side
+			ahead(150);
+			turnRight(90);
+			//second side
+			ahead(150);
+			turnRight(90);
+			//third side
+			ahead(150);
+			turnRight(90);
+			//fourth side
+			ahead(150);
+			turnRight(90);	
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
