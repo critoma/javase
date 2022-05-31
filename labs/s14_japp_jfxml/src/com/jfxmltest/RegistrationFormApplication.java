4f9209f13fbd6f0813546175c313ee1e5f22a6fb
critoma <cristianvtoma@gmail.com>
2022-06-01 00:44:34 +0300

    Update RegistrationFormApplication.java

--- labs/s14_japp_jfxml/src/com/jfxmltest/RegistrationFormApplication.java
+++ labs/s14_japp_jfxml/src/com/jfxmltest/RegistrationFormApplication.java
@@ -10,6 +10,25 @@ import java.io.FileInputStream;
 //import java.net.URL;
 //import javafx.scene.layout.BorderPane;
 
+// SQLite: https://github.com/xerial/sqlite-jdbc/releases
+// Java-JSON: https://github.com/stleary/JSON-java | https://search.maven.org/remotecontent?filepath=org/json/json/20220320/json-20220320.jar
+// for GraalVM JS: see Maven file from lib/pom.xml
+
+/*
+ * Add *.jar files from lib folder of the JavaFX SDK: https://github.com/critoma/javase/tree/master/tools/JavaFX
+ * 
+ * in run configuration dialog add the following:
+ * --module-path "\path to javafx\lib" --add-modules javafx.controls,javafx.fxml
+ * 
+ * e.g. for Windows (FXML files and *.dll files from JavaFX bin folder in the root of project):
+ * --module-path ".\lib" --add-modules javafx.controls,javafx.fxml
+ * 
+ * * e.g. for MacOS/Linux (FXML files in the root of project, but *.dylib/*.so from JavaFX bin directory in lib folder):
+ * --module-path ".\lib" --add-modules javafx.controls,javafx.fxml
+ * -Dpolyglot.engine.WarnInterpreterOnly=false
+ * /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home/bin/java --module-path ./lib --add-modules javafx.controls,javafx.fxml -cp /Users/ctoma/eclipse-workspace-jee2022/s14_japp_fxml17/bin com.jfxmltest.RegistrationFormApplication 
+ */
+
 public class RegistrationFormApplication extends Application {
 
     @Override
