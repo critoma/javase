dd9e5ed7ed2d52992d34b8a61d9eb8df5ebb1ffb
critoma <cristianvtoma@gmail.com>
2024-02-26 08:04:26 +0200

    Create ProgMainOopArraysCar.java

---
+++ lectures/c02/src/eu/ase/ooparrays/ProgMainOopArraysCar.java
@@ -0,0 +1,18 @@
+package eu.ase.ooparrays;
+
+public class ProgMainOopArraysCar {
+
+	public static void main(String[] args) {
+		Car.setNoCars(2);
+		Car[] carsArray = new Car[Car.getNoStud()];
+
+		carsArray[0] = new Car("C0", new short[] {51, 50, 92});
+		carsArray[1] = new Car("S1", new short[] {94, 10, 19});
+		
+		for (int i = 0; i < Car.getNoCars(); i++) {
+			System.out.println("The average km for the car "+ i +" is = "+
+					carsArray[i].getAverageKm());
+		} // end for statment
+	} // end main method
+
+} // end class
