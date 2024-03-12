eeeb4911ef7a4d70615ed0e43f086b8297969dc6
critoma <cristianvtoma@gmail.com>
2024-03-12 12:16:46 +0200

    Update ProgMainJCF0.java

--- lectures/c04/src/eu/ase/jcf/ProgMainJCF0.java
+++ lectures/c04/src/eu/ase/jcf/ProgMainJCF0.java
@@ -6,12 +6,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.Vector;
 
-class Patient implements Cloneable {
+class Human {
+	private int ssn; // cnp; <=> social security number
+	public Human(int cnp) {
+		this.ssn = cnp;
+	}
+	void print() {
+        System.out.println("Human - ssn:" + this.ssn);
+    }
+	public int getSsn() { return ssn; }
+}
+
+class Student extends Human {
+	private int group;
+	public Student(int cnp, int group) {
+		super(cnp);
+		this.group = group;
+	}
+	void print() {
+        System.out.println("Student - ssn:" + this.getSsn() + ", group = " + group);
+    }
+}
+
+class Patient extends Human implements Cloneable {
     private int id;
     private String name;
     
-    public Patient(int idPatient, String name) {
+    public Patient(int cnp, int idPatient, String name) {
+    	super(cnp);
         id = idPatient;
         this.name = name;
     }
@@ -55,59 +79,91 @@ class Hospital implements Comparable<Hospital> {
 }
 
 public class ProgMainJCF0 {
-
 	public static void main(String[] args) {
+		// PLEASE draw the abstract memory layout:
+		
+		// clone:
+		Patient objp1 = new Patient(1, 12, "Ion");
+		Patient objp2 = new Patient(2, 15, "Dan");
+		try {
+			objp2 = (Patient) objp1.clone();
+			objp1.print();
+			objp2.print();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		// runtime/"pure" poly-morphism:
+		Human objh = null;
+		objh = new Student(1, 1003);
+		objh.print();
+		// 1000 lines of code:
+		objh = new Patient(7, 22, "Ana");
+		objh.print();
+		
+		// class cast exception:
+		Human human5 = null;
+		Human student5 = new Student(55, 1003);
+		human5 = student5;
+		try {
+			Patient patient5 = (Patient) human5;
+			patient5.print();
+		} catch (ClassCastException cce) {
+			cce.printStackTrace();
+		}
+		
+		
+		// JCF - Java Collection Framework:
 		List<Patient> listP = new LinkedList<Patient>();
-        for(int i = 0; i < 5; i++) 
-        	listP.add(new Patient(200+i, "Location "+i));
+        	for(int i = 0; i < 5; i++) 
+        		listP.add(new Patient(100+i, 200+i, "Location "+i));
         
-        System.out.println("Normal iteration:");
-        for(int i = 0; i < listP.size(); i++) {
-            Patient pTemp = null;
-            pTemp = listP.get(i);
-            pTemp.print();
-        }
-        System.out.println("for-each iteration:");
-        for(var pTemp : listP) {
-        	pTemp.print();
-        }
-        System.out.println("Iteration with iterator object:");
-        for(Iterator<Patient> it = listP.iterator(); it.hasNext();) {
-            Patient pTemp = it.next();
-            pTemp.print();
-        }   
+	        System.out.println("Normal iteration:");
+	        for(int i = 0; i < listP.size(); i++) {
+	            Patient pTemp = null;
+	            pTemp = listP.get(i);
+	            pTemp.print();
+	        }
+	        System.out.println("for-each iteration:");
+	        for(var pTemp : listP) {
+	        	pTemp.print();
+	        }
+	        System.out.println("Iteration with iterator object:");
+	        for(Iterator<Patient> it = listP.iterator(); it.hasNext();) {
+	            Patient pTemp = it.next();
+	            pTemp.print();
+	        }   
         
-        Map<Hospital, List<Patient>> arb = new TreeMap<Hospital, List<Patient>>(); 
-        // Map<Hospital, List<Patient>> arb = new HashMap<Hospital, List<Patient>>(); 
-        for(int i = 0; i < 7; i++) {
-        	Hospital hk = new Hospital(200+i, 100, 30, "Location: " + i);
-        	//List<Patient> plv = listP.subList(0, listP.size());
-        	
-        	@SuppressWarnings("unchecked")
+	        Map<Hospital, List<Patient>> arb = new TreeMap<Hospital, List<Patient>>(); 
+	        // Map<Hospital, List<Patient>> arb = new HashMap<Hospital, List<Patient>>(); 
+	        for(int i = 0; i < 7; i++) {
+	        	Hospital hk = new Hospital(200+i, 100, 30, "Location: " + i);
+	        	//List<Patient> plv = listP.subList(0, listP.size());
+	        	
+	        	@SuppressWarnings("unchecked")
 			List<Patient> plv = ((List<Patient>) ((LinkedList<Patient>) (listP) ).clone());
-        	for(var idx = 0; idx < plv.size(); idx++) {
-                Patient pTemp = null;
+	        	for(var idx = 0; idx < plv.size(); idx++) {
+	                	Patient pTemp = null;
 				try {
 					pTemp = (Patient)listP.get(idx).clone();
 				} catch (CloneNotSupportedException e) {
 					e.printStackTrace();
 				}
-                plv.set(idx, pTemp);
-            }
-            arb.put(hk, plv);
-        }
-        
-        System.out.println("\n\nPrint associative data structure:");    
-        System.out.println(arb);
-        Set<Hospital> s = arb.keySet();
-        Iterator<Hospital> ith = s.iterator();
-        for(;ith.hasNext();) {
-            Hospital hk = ith.next();
-            List<Patient> plvalue = arb.get(hk);
-            hk.print(); 
-            for(var objP : plvalue) 
-            	objP.print();
-        }
+	                	plv.set(idx, pTemp);
+	            	}
+	            	arb.put(hk, plv);
+	        }
+	        
+	        System.out.println("\n\nPrint associative data structure:");    
+	        System.out.println(arb);
+	        Set<Hospital> s = arb.keySet();
+	        Iterator<Hospital> ith = s.iterator();
+	        for(;ith.hasNext();) {
+	            Hospital hk = ith.next();
+	            List<Patient> plvalue = arb.get(hk);
+	            hk.print(); 
+	            for(var objP : plvalue) 
+	            	objP.print();
+	        }
 	} // end main method
 
 } // end main class
