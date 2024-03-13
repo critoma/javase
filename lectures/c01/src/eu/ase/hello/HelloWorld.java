083182d2a685bef08ae44b4203f735641052bdd4
critoma <cristianvtoma@gmail.com>
2024-03-13 11:24:45 +0200

    Update HelloWorld.java

--- lectures/c01/src/eu/ase/hello/HelloWorld.java
+++ lectures/c01/src/eu/ase/hello/HelloWorld.java
@@ -6,9 +6,32 @@ public class HelloWorld
  {
         int r = 0; int a = 9; int b = -2;
         r = a + b;
-        System.out.println("My first Java! r = " + r);
+	/*
+        if (r > 0)
+    		r++;
+        else
+        	r = 0;
+
+        int idx = 0;
+	while(idx < 3) {
+	    	System.out.println("idx = " + idx);
+	    	idx++;
+        }
+
+        HelloWorld objH = new HelloWorld();
+        objH.toString();
+        */
+        // try (RandomAccessFile raf = new RandomAccessFile("./test.bin.txt", "rw"); ) {
+        //	int x = 42; // 0x2A;
+        //	raf.writeByte(x);
+        // } catch(IOException ioe) {
+        // }
+	 
+        System.out.println("Hello World form Java!!! r = " + r);
  }
 }
+
+
 
 /*
 public class HelloWorld
