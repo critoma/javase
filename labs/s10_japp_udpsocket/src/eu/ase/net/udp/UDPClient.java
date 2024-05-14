da6485a88d4f28e75f2d960a97a9a4f22670ed42
critoma <cristianvtoma@gmail.com>
2024-05-14 09:53:33 +0300

    Update UDPClient.java

--- labs/s10_japp_udpsocket/src/eu/ase/net/udp/UDPClient.java
+++ labs/s10_japp_udpsocket/src/eu/ase/net/udp/UDPClient.java
@@ -6,33 +6,35 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPClient {
-	public static void main(String[] args) throws IOException {
-		// get a datagram socket
-		DatagramSocket clientSocket = new DatagramSocket();
-		
-		// send the request
-		byte[] buf = new byte[256];
-		buf[0] = 'H'; buf[1] = 'e'; buf[2] = 'l';
-		buf[3] = 'l'; buf[4] = 'o';
-		// buf = new String("Hello").getBytes();
 
-		//InetAddress address = InetAddress.getByName("127.0.0.1");
-		InetAddress address = InetAddress.getByName(args[0]);
-		int port = Integer.parseInt(args[1]); // 7778
-		DatagramPacket packet = 
-				new DatagramPacket(buf, buf.length, address, port);
-		clientSocket.send(packet);
+	public static void udpClient(String[] args) throws IOException {
+		DatagramSocket clientSock = new DatagramSocket();
 		
-		// get the response
+		// byte[] buf = new byte[256];
+		// buf[0] = 'H'; buf[1] = 'e'; buf[2] = 'l'; buf[3] = 'l'; buf[4] = 'o';
+		// buf = (new String("Hello")).getBytes();
+		byte[] buf = new String("What date & time is it?").getBytes();
+		
+		// java -cp . eu.deic.net.UDPClient 127.0.0.1 7778
+		InetAddress dstAddr = InetAddress.getByName(args[0]); // InetAddress.getByName("127.0.0.1");
+		int dstPort = Integer.parseInt(args[1]);
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, dstAddr, dstPort);
+		clientSock.send(packet);
+		
 		byte[] bufResp = new byte[256];
-		packet = new DatagramPacket(bufResp, bufResp.length);
-		clientSocket.receive(packet);
+		DatagramPacket packetRecvFromServer = new DatagramPacket(bufResp, bufResp.length);
+		clientSock.receive(packetRecvFromServer);
+		System.out.println("Client received from Server = " + new String(packetRecvFromServer.getData()));
 		
-		// display the response
-		String received = new String(packet.getData());
-		System.out.println("Client from server received = " + received);
-		
-		// close socket
-		clientSocket.close();
-	}
-}
+		clientSock.close();
+	} // end method udpClient
+	
+	public static void main(String[] args) {
+		try {
+			udpClient(args);
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+	} // end main method
+
+} // end UDPClient class
