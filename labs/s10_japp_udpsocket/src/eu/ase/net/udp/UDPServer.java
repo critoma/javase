efd2535c6ca8614ce29456e2adcfa4b832af29b3
critoma <cristianvtoma@gmail.com>
2024-05-14 09:55:23 +0300

    Update UDPServer.java

--- labs/s10_japp_udpsocket/src/eu/ase/net/udp/UDPServer.java
+++ labs/s10_japp_udpsocket/src/eu/ase/net/udp/UDPServer.java
@@ -4,46 +4,47 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Date;
 
 public class UDPServer {
+
 	public static void main(String[] args) {
-		//DatagramSocket socket = null;
-		byte[] bufRecv = null; byte[] bufResp = null;
+		byte[] bRecv = null; byte[] bResp = null;
 		
 		// re-factored with "try with resources"
 		//try {
 			//socket = new DatagramSocket(7778);
 		try (DatagramSocket socket = new DatagramSocket(7778)) {
-			System.out.println("UDP Server bind on 7778 port.");
-			while (true) {
-				bufRecv = new byte[256];
-				
-				// receive request
-				DatagramPacket packet = 
-						new DatagramPacket(bufRecv, bufRecv.length);
+			System.out.println("My UDP DEIC/DICE Server is bining in port 7778");
+			while(true) {
+				bRecv = new byte[256];
+				DatagramPacket packet = new DatagramPacket(bRecv, bRecv.length);
 				socket.receive(packet);
 				
-				//bufRecv = packet.getData();
-				System.out.println("Client says: " + new String(bufRecv));
+				System.out.println("UDP Client " + packet.getAddress() + ":" + packet.getPort() + " sent 2 Server = " + new String(packet.getData()) 
+						/*new String(bRecv)*/ );
 				
-				// figure out the response
-				String respString = new String("OK");
-				bufResp = respString.getBytes();
+				String respS = null;
+				if ( "What date & time is it?".equals( new String(packet.getData() ).trim() )  ) {
+					respS = new Date().toString();
+				} else {
+					respS = new String("I don't understand!");
+				}
 				
-				// send the response at the client address and port
-				InetAddress address = packet.getAddress();
-				int port = packet.getPort();
-				DatagramPacket packetResp = 
-						new DatagramPacket(bufResp, bufResp.length, address, port);
-				socket.send(packetResp);
+				bResp = respS.getBytes();
 				
+				InetAddress addrSender = packet.getAddress();
+				int portSender = packet.getPort();
+				DatagramPacket respPacket = new DatagramPacket(bResp, bResp.length, addrSender, portSender);
+				socket.send(respPacket);
 			} // end while
 		} catch(IOException ioe) {
 			ioe.printStackTrace();
-		} 
+		}
 //		finally {
 //			if (socket != null) 
 //				socket.close();
 //		}
-	} // end main
+	} // end main method
+
 } // end class
